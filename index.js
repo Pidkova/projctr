7851362235069aa227d1796f396476837a982f6d
Pidkova <parmelion12@gmail.com>
2022-12-02 16:16:00 +0100

    Update

--- index.js
+++ index.js
@@ -101,34 +101,6 @@ console.log(iterativeOddSumTo(10)); //25
 //
 //
 //
-/*----Домашнє завдання за 18.11.2022------*/
-//
-//1. Напишіть функцію `addThemAll` яка буде знаходити сумму усіх своїх аргументів незалежно від їх кількості (але без використання вбутованого об'єкту Math).
-//Використайте оператор розширення:
-
-function addThemAll(...args) {
-  let sum = 0;
-
-  for (let arg of args) sum += arg;
-  return sum;
-}
-
-console.log(addThemAll(2, 4)); // 6
-console.log(addThemAll(1, 2, 3, 4)); // 10
-console.log(addThemAll(5, 5, 10)); // 20
-
-//2. Задача на використання замикання. Напишіть функцію яка працює таким чином:
-/*---`multiply(a)(b)` // a * b ---*/
-
-console.log(multiply(5)(5)); // 25
-console.log(multiply(2)(-2)); // -4
-console.log(multiply(4)(3)); // 12
-
-function multiply(a) {
-  return function (b) {
-    return a * b;
-  };
-}
 
 /*----Домашнє завдання за 18.11.2022------*/
 //
